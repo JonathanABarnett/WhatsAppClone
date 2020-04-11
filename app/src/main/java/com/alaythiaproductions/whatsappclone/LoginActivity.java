74bea5306779b080dc8a384378aa067862fb26ec
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-11 15:06:00 -0500

    Create Login and Register layouts

---
+++ app/src/main/java/com/alaythiaproductions/whatsappclone/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.alaythiaproductions.whatsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser != null) {
+            sendUserToMainActivity();
+        }
+    }
+
+    private void sendUserToMainActivity() {
+        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(loginIntent);
+        finish();
+    }
+}
--- app/src/main/java/com/alaythiaproductions/whatsappclone/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/whatsappclone/MainActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,11 +18,16 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar mToolbar;
     private ViewPager2 viewPager;
 
+    private FirebaseUser currentUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /**
+         *
+         */
          mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("What's Up");
@@ -48,5 +55,20 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         tabLayoutMediator.attach();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser == null) {
+            sendUserToLoginActivity();
+        }
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
