bc9475a53b009d4764217c9c74e746a93fce3cd5
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-11 15:17:02 -0500

    Set intent for Login and Register pages

--- app/src/main/java/com/alaythiaproductions/whatsappclone/LoginActivity.java
+++ app/src/main/java/com/alaythiaproductions/whatsappclone/LoginActivity.java
@@ -5,17 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
+    private Button loginButton, phoneLoginButton;
+    private EditText userEmail, userPassword;
+    private TextView needNewAccountLink, forgotPasswordLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeFields();
+
+        needNewAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToRegisterActivity();
+            }
+        });
     }
 
     @Override
@@ -31,5 +49,20 @@ public class LoginActivity extends AppCompatActivity {
         Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(loginIntent);
         finish();
+    }
+
+    private void sendUserToRegisterActivity() {
+        Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+        startActivity(registerIntent);
+        finish();
+    }
+
+    private void initializeFields() {
+        loginButton = findViewById(R.id.login_button);
+        phoneLoginButton = findViewById(R.id.login_phone_button);
+        userEmail = findViewById(R.id.login_email);
+        userPassword = findViewById(R.id.login_password);
+        needNewAccountLink = findViewById(R.id.login_sign_up);
+        forgotPasswordLink = findViewById(R.id.login_forgot_password);
     }
 }
--- app/src/main/java/com/alaythiaproductions/whatsappclone/RegisterActivity.java
+++ app/src/main/java/com/alaythiaproductions/whatsappclone/RegisterActivity.java
@@ -2,13 +2,44 @@ package com.alaythiaproductions.whatsappclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private Button registerButton;
+    private EditText userEmail, userPassword;
+    private TextView alreadyHaveAccountLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        initializeFields();
+
+        alreadyHaveAccountLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendUserToLoginActivity();
+            }
+        });
+    }
+
+    private void initializeFields() {
+        registerButton = findViewById(R.id.register_button);
+        userEmail = findViewById(R.id.register_email);
+        userPassword = findViewById(R.id.register_password);
+        alreadyHaveAccountLink = findViewById(R.id.register_already_have_account);
+    }
+
+    private void sendUserToLoginActivity() {
+        Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(loginIntent);
+        finish();
     }
 }
