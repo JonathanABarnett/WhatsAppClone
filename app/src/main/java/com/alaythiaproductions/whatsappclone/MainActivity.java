19a0b5b69d5e7ab851f4c3ad79415385cdf3181b
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-04-11 14:12:25 -0500

    Set up tabs, add 3 fragments, colors need to be changed

--- app/src/main/java/com/alaythiaproductions/whatsappclone/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/whatsappclone/MainActivity.java
@@ -1,22 +1,52 @@
 package com.alaythiaproductions.whatsappclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mToolbar = findViewById(R.id.main_page_toolbar);
+         mToolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("What's Up");
+
+        viewPager = findViewById(R.id.main_tabs_pager);
+        viewPager.setAdapter(new TabsPagerAdapter(this));
+
+        TabLayout tabLayout = findViewById(R.id.main_tabs);
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(
+                tabLayout, viewPager, (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("Chats");
+                            tab.setIcon(R.drawable.ic_check);
+                            break;
+                        case 1:
+                            tab.setText("Groups");
+                            tab.setIcon(R.drawable.ic_paint);
+                            break;
+                        case 2:
+                            tab.setText("Contacts");
+                            tab.setIcon(R.drawable.ic_pending);
+                            break;
+                    }
+                }
+        );
+        tabLayoutMediator.attach();
     }
 }
---
+++ app/src/main/java/com/alaythiaproductions/whatsappclone/TabsPagerAdapter.java
@@ -0,0 +1,37 @@
+package com.alaythiaproductions.whatsappclone;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class TabsPagerAdapter extends FragmentStateAdapter {
+
+    public TabsPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                ChatsFragment chatsFragment = new ChatsFragment();
+                return chatsFragment;
+            case 1:
+                GroupsFragment groupsFragment = new GroupsFragment();
+                return groupsFragment;
+            case 2:
+                ContactsFragment contactsFragment = new ContactsFragment();
+                return contactsFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+}
